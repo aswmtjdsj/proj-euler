9b50567b5452228ab77a502c140d39e903f8d07c
Parlin.Yu <bupt.aswmtjdsj@gmail.com>
2014-03-26 03:18:17 +0800

    102,notdone

---
+++ 102.py
@@ -0,0 +1,24 @@
+import sys, os
+
+def left(p, q, s):
+    # (s - p) ^ (q - p)
+    ax = s[0] - p[0]
+    ay = s[1] - p[1]
+    bx = q[0] - p[0]
+    by = q[1] - p[1]
+    # print ax, ay, bx, by, ax * by - ay * bx
+    return ax * by - ay * bx > 0
+
+o = (0, 0)
+cnt = 0
+for line in sys.stdin:
+    p = map(int, line.replace('\n', '').split(','))
+    a = (p[0],p[1])
+    b = (p[2],p[3])
+    c = (p[4],p[5])
+    if (left(a, b, c) and left(b, c, o) and left(c, a, o)) or  (left(b, a, c) and left(a, c, o) and left(c, b, o)):
+        cnt += 1
+        # print a, b, c, cnt
+        # raw_input()
+
+print cnt
