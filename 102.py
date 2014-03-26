ce729db8611cb224faf2f4822fe468a06ce9060e
Parlin.Yu <bupt.aswmtjdsj@gmail.com>
2014-03-26 19:52:49 +0800

    102 updated and i'm too sb

--- 102.py
+++ 102.py
@@ -2,10 +2,10 @@ import sys, os
 
 def left(p, q, s):
     # (s - p) ^ (q - p)
-    ax = s[0] - p[0]
-    ay = s[1] - p[1]
-    bx = q[0] - p[0]
-    by = q[1] - p[1]
+    ax = q[0] - p[0]
+    ay = q[1] - p[1]
+    bx = s[0] - p[0]
+    by = s[1] - p[1]
     # print ax, ay, bx, by, ax * by - ay * bx
     return ax * by - ay * bx > 0
 
@@ -16,9 +16,9 @@ for line in sys.stdin:
     a = (p[0],p[1])
     b = (p[2],p[3])
     c = (p[4],p[5])
-    if (left(a, b, c) and left(b, c, o) and left(c, a, o)) or  (left(b, a, c) and left(a, c, o) and left(c, b, o)):
+    if (left(a, b, c) and left(b, c, o) and left(c, a, o) and left(a, b, o)) or (left(b, a, c) and left(a, c, o) and left(c, b, o) and left(b, a, o)):
         cnt += 1
-        # print a, b, c, cnt
+        print a, b, c, cnt
         # raw_input()
 
 print cnt
